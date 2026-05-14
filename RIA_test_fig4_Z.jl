ea4fe7c472225df76060b8695486ad0b21d24532
FlorenceRiv <florenceriverin@gmail.com>
2026-05-14 12:11:12 -0400

    Z
    Implement DDCS integration over mu for DCS(E) calculation.

---
+++ RIA_test_fig4_Z.jl
@@ -0,0 +1,215 @@
+"""
+integrating DDCS(E,mu) over mu to get DCS(E)
+fig 4 Brusa paper
+"""
+
+using Plots
+using QuadGK
+using FastGaussQuadrature, LinearAlgebra
+
+include("Radiant.jl/src/cross_sections/electron_subshells.jl")
+include("Radiant.jl/src/cross_sections/orbital_compton_profiles.jl")
+
+Z = 13 # atomic number (Al)
+E_keV = 500. # keV. initial photon energy
+x_start = 0.2
+
+# number of nodes for Gauss quadrature
+nodes = 50
+# Gauss-Jacobi alpha, beta > -1
+alpha = 1/3 
+beta = -1/3
+
+_, Zi, Ui, _, _, _ = electron_subshells(Z,false)
+    """
+    electron_subshells(Z::Int64,is_free::Bool=false)
+    - `Z::Int64`: atomic number of the element.
+    - `is_free::Bool` : are the electrons free (not bounded) ?
+    
+    Conversion of Ui in the function
+        mₑc² = 0.510999  # MeV
+        Ui ./= 1e6       # eV → MeV
+        Ui ./= mₑc²      # MeV → mₑc²
+
+    return Nshells,Zi,Ui,Ti,ri,subshells
+    """
+
+Jio = orbital_compton_profiles(Z)
+    """
+    return J₀ 
+    in ħ/(mₑe²) → atomic units
+    """
+# divide by fine structure constant to match E_z units
+Jio .*= 137.035999177 
+
+# E unit conversion
+E = E_keV
+mₑc² = 0.510999 # MeV
+E /= 1e3 # keV → MeV
+E /= mₑc² # MeV → mₑc²
+
+E_0 = 1. # (mₑc²) rest mass energy of electron 
+r_e = 2.8179e-13 # (cm) electron radius
+
+
+function calc_ddcs_ria(E_f, mu)
+
+    E_q = sqrt(E^2 + E_f^2 - 2*E*E_f*mu)
+
+    E_z = (E*E_f*(1-mu) - E_0*(E - E_f)) / E_q
+
+    E_c = E_0*E / (E_0 + E*(1-mu)) 
+
+    F_Emu = (E_0*E*E_f / (E_q * E_c^2)) * inv(sqrt(1 + (E_z/E_0)^2))
+
+    summation = 0.
+
+    for i in eachindex(Ui)
+
+        if E - E_f > Ui[i] # Heaviside. otherwise = 0
+
+            Ji = 
+                Jio[i] * 
+                (1+2*Jio[i]*abs(E_z)) * 
+                exp((1-(1+2*Jio[i]*abs(E_z))^2)/2)
+
+            summation += Zi[i]*Ji
+
+        end
+    end
+
+    DDCS = 
+            (r_e^2 /2) * 
+            (E_c/E)^2 * 
+            (E_c/E +E/E_c + mu^2 - 1) * 
+            F_Emu * 
+            summation *
+            1e24 # cm2 to barn
+
+    return DDCS # Omega and E_f derivative of CS
+end
+
+# quadgk integration (time inefficient)
+function quadgk_integrate_ddcs_over_mu(E_f)
+    
+    integral, error = quadgk(mu -> calc_ddcs_ria(E_f, mu), -1, 1)
+
+    return integral
+end
+
+# ===== the next functions are from FastGaussQuadrature.jl =====
+
+# Gauss-Legendre quadrature integration (faster but is it precise for this case?)
+function GaussLegendre_integrate_ddcs_over_mu(E_f)
+    
+    mu, weights = gausslegendre(nodes)
+
+    integral = dot(weights, calc_ddcs_ria.(E_f, mu))
+
+    return integral
+end
+
+function GaussChebyshev1_integrate_ddcs_over_mu(E_f)
+    
+    mu, weights = gausschebyshevt(nodes)
+
+    integral = dot(weights, calc_ddcs_ria.(E_f, mu).*sqrt.(1 .- mu.^2)) 
+    # divide function by weight w1(x) = 1/sqrt(1-x^2)
+
+    return integral
+end
+
+function GaussChebyshev2_integrate_ddcs_over_mu(E_f)
+    
+    mu, weights = gausschebyshevu(nodes)
+
+    integral = dot(weights, calc_ddcs_ria.(E_f, mu)./sqrt.(1 .- mu.^2))
+    # divide function by weight w2(x) = sqrt(1-x^2)
+
+    return integral
+end
+
+function GaussChebyshev3_integrate_ddcs_over_mu(E_f)
+    
+    mu, weights = gausschebyshevv(nodes)
+
+    integral = dot(weights, calc_ddcs_ria.(E_f, mu).*sqrt.((1 .- mu)./(1 .+ mu)))
+    # divide function by weight w3(x) = sqrt((1 + mu)/(1 - mu))
+
+    return integral
+end
+
+function GaussChebyshev4_integrate_ddcs_over_mu(E_f)
+    
+    mu, weights = gausschebyshevw(nodes)
+
+    integral = dot(weights, calc_ddcs_ria.(E_f, mu).*sqrt.((1 .+ mu)./(1 .- mu)))
+    # divide function by weight w4(x) = sqrt((1 - mu)/(1 + mu))
+
+    return integral
+end
+
+function GaussJacobi_integrate_ddcs_over_mu(E_f, alpha, beta)
+    
+    mu, weights = gaussjacobi(nodes, alpha, beta)
+
+    integral = dot(weights, calc_ddcs_ria.(E_f, mu)./((1 .- mu).^alpha.*(1 .+ mu).^beta))
+    # divide function by weight w(x) = (1-x)^alpha * (1+x)^beta
+
+    return integral
+end
+
+function GaussRadau_integrate_ddcs_over_mu(E_f)
+
+    mu, weights = gaussradau(nodes)
+
+    integral = dot(weights, calc_ddcs_ria.(E_f, mu))
+
+    return integral
+end
+
+function GaussLobatto_integrate_ddcs_over_mu(E_f)
+
+    mu, weights = gausslobatto(nodes)
+
+    integral = dot(weights, calc_ddcs_ria.(E_f, mu))
+
+    return integral
+end
+
+
+# array of E_f values
+E_f_vals = range(x_start*E, E, length=200) # paper starts at 0.7E
+
+# x axis
+xvals = E_f_vals ./ E
+# y axis
+quadgk_yvals = (E/Z)*2*pi*[quadgk_integrate_ddcs_over_mu(E_f) for E_f in E_f_vals]
+GaussLegendre_yvals = (E/Z)*2*pi*[GaussLegendre_integrate_ddcs_over_mu(E_f) for E_f in E_f_vals]
+GaussChebyshev1_yvals = (E/Z)*2*pi*[GaussChebyshev1_integrate_ddcs_over_mu(E_f) for E_f in E_f_vals]
+GaussChebyshev2_yvals = (E/Z)*2*pi*[GaussChebyshev2_integrate_ddcs_over_mu(E_f) for E_f in E_f_vals]
+GaussChebyshev3_yvals = (E/Z)*2*pi*[GaussChebyshev3_integrate_ddcs_over_mu(E_f) for E_f in E_f_vals]
+GaussChebyshev4_yvals = (E/Z)*2*pi*[GaussChebyshev4_integrate_ddcs_over_mu(E_f) for E_f in E_f_vals]
+GaussJacobi_yvals = (E/Z)*2*pi*[GaussJacobi_integrate_ddcs_over_mu(E_f, alpha, beta) for E_f in E_f_vals]
+GaussRadau_yvals = (E/Z)*2*pi*[GaussRadau_integrate_ddcs_over_mu(E_f) for E_f in E_f_vals]
+#GaussLobatto_yvals = (E/Z)*2*pi*[GaussLobatto_integrate_ddcs_over_mu(E_f) for E_f in E_f_vals] # diverges when few nodes
+
+Plots.plot(
+    xvals, quadgk_yvals,
+    xlabel = "E' / E",
+    ylabel = raw"$\frac{E}{Z}\frac{d\sigma}{dE}$ (barn)",
+    label = "quadgk",
+    title = "DCS(E') for Al, E = $E_keV keV, $nodes nodes",
+    legend=:bottom
+)
+#=
+Plots.plot!(xvals, GaussLegendre_yvals, label = "GaussLegendre")
+Plots.plot!(xvals, GaussChebyshev1_yvals, label = "GaussChebyshev1")
+Plots.plot!(xvals, GaussChebyshev2_yvals, label = "GaussChebyshev2")
+Plots.plot!(xvals, GaussChebyshev3_yvals, label = "GaussChebyshev3")
+Plots.plot!(xvals, GaussChebyshev4_yvals, label = "GaussChebyshev4")
+a=round(alpha, digits=2); b=round(beta, digits=2)
+Plots.plot!(xvals, GaussJacobi_yvals, label = "GaussJacobi, α=$a, β=$b")
+Plots.plot!(xvals, GaussRadau_yvals, label = "GaussRadau")
+#Plots.plot!(xvals, GaussLobatto_yvals,label = "GaussLobatto")
+=#
