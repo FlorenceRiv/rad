1f1588545ee827c6b6e34db1332834c4048e90ec
FlorenceRiv <florenceriverin@gmail.com>
2026-05-13 20:41:44 -0400

    Refactor DDCS calculation and update energy units
    Refactor the calculation of differential cross sections and update unit conversions for energy. Added support for multiple scattering angles.

--- RIA_test_fig2.jl
+++ RIA_test_fig2.jl
@@ -9,53 +9,49 @@ include("Radiant.jl/src/cross_sections/electron_subshells.jl")
 include("Radiant.jl/src/cross_sections/orbital_compton_profiles.jl")
 
 Z = 13 # atomic number (Al)
-E = 10. # keV. initial photon energy
+E_keV = 10. # keV. initial photon energy
 
 _, Zi, Ui, _, _, _ = electron_subshells(Z,false)
-    """  function info:
-
+    """
     electron_subshells(Z::Int64,is_free::Bool=false)
     - `Z::Int64`: atomic number of the element.
     - `is_free::Bool` : are the electrons free (not bounded) ?
     
-    Conversions
+    Conversion of Ui in the function
         mₑc² = 0.510999  # MeV
         Ui ./= 1e6       # eV → MeV
-        Ti ./= 1e6       # eV → MeV
-        ri .*= 1e-11     # mÅ → cm
-        Nshells = length(Zi)
         Ui ./= mₑc² # MeV → mₑc²
-        Ti ./= mₑc² # MeV → mₑc² 
 
     return Nshells,Zi,Ui,Ti,ri,subshells
     """
 
 Jio = orbital_compton_profiles(Z)
     """
-    orbital_compton_profiles(Z::Int64)
-    - `Z::Int64` : atomic number of the element.
-
     return J₀ 
-    [in ħ/(mₑe²) → atomic units].
+    in ħ/(mₑe²) → atomic units
     """
-#=
+# attempt at converting units (kills the curve)
+"""
 alpha = 0.0072973525643 # sommerfeld constant = e²/(ħc)
 c = 2.99792458e8 # m/s
 Jio .*= (c / alpha ) # ħ/(mₑe²) → 1/mₑc²
-=#
+"""
 
-# normalisation E
+# E unit conversion
 mₑc² = 0.510999 # MeV
+E = E_keV
 E /= 1e3 # keV → MeV
 E /= mₑc² # MeV → mₑc²
 
 E_0 = 0.510999 # MeV rest mass energy of electron (mₑc²)
-theta = 60 # degrees
-mu = cosd(theta)
+theta1 = 60 # degrees
+theta2 = 180
+mu1 = cosd(theta1)
+mu2 = cosd(theta2)
 r_e = 2.8179e-13 # (cm) electron radius
 
 
-function calc_ddcs_ria(E_f)
+function calc_ddcs_ria(E_f, mu)
 
     E_q = sqrt(E^2 + E_f^2 - 2*E*E_f*mu)
 
@@ -63,7 +59,7 @@ function calc_ddcs_ria(E_f)
 
     E_c = E_0*E / (E_0 + E*(1-mu)) 
 
-    F_Emu = (E_0*E*E_f / E_q * E_c^2) * (1 + (E_z/E_0)^2)^(-1/2)
+    F_Emu = (E_0*E*E_f / E_q * E_c^2) * inv(sqrt(1 + (E_z/E_0)^2))
 
     summation = 0.
 
@@ -92,17 +88,23 @@ function calc_ddcs_ria(E_f)
 
 end
 
+# array of E_f values
+E_f_values = range(0.9*E, E, length=200)
 
-E_f_values = range(0, E, length=200)
-
+# x axis
 xvals = E_f_values ./ E
-
-yvals = [calc_ddcs_ria(E_f) for E_f in E_f_values]
-
+# y axis
+yvals1 = [calc_ddcs_ria(E_f, mu1) for E_f in E_f_values]
+yvals2 = [calc_ddcs_ria(E_f, mu2) for E_f in E_f_values]
 
 Plots.plot(
     xvals, yvals,
 
     xlabel = "E_f / E",
-    ylabel = "DDCS (cm²)"
+    ylabel = "DDCS (cm²)",
+
+    label = "θ = 60°",
+    title = "Al DDCS(E'), E = $E_keV keV"
 )
+
+Plots.plot!(xvals, yvals2, label = "θ = 180°")
