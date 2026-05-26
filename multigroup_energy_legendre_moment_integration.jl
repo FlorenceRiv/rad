8074220f902c1d2a89ba148decb28d6f31d557af
FlorenceRiv <florenceriverin@gmail.com>
2026-05-26 18:11:11 -0400

    works for specific energy now
    Updated variable names for clarity and consistency, including changing Legendre order and photon energy group indices. Adjusted function parameters to include atomic subshell information.

--- multigroup_energy_legendre_moment_integration.jl
+++ multigroup_energy_legendre_moment_integration.jl
@@ -9,9 +9,9 @@ definitions:
     - Ef : outgoing photon energy in mₑc²
     - Ui : binding energy of subshell i in mₑc²
     - Jio : part of orbital compton profile of subshell i in mₑc²
-    - l : Legendre order
-    - g : incoming photon energy group index
-    - g_f : outgoing photon energy group index
+    - l : Legendre moment order
+    - g : initial photon energy group index
+    - g_f : outgoing photon energy group indexa
     - Z : atomic number (Al)
     - E_max : (keV) midpoint energy of the highest energy group.
     - E_min : (keV) cutoff energy for multigroup structure
@@ -25,23 +25,22 @@ using Radiant
 include("Radiant.jl/src/cross_sections/electron_subshells.jl")
 include("Radiant.jl/src/cross_sections/orbital_compton_profiles.jl")
 include("Radiant.jl/src/cross_sections/energy_group_structure.jl")
-include("recurrent_functions.jl") # my functions
+include("recurrent_functions.jl") 
 
+Z = 13 # (Al)
+E_max_keV = 50.
+E_min_keV = 0.01
+Ef_max_keV = 50. 
+Ef_min_keV = 0.01
 
-Z = 13 # atomic number (Al)
-E_max = 50. # keV. midpoint energy of the highest energy group.
-E_min = 0.01 # keV. cutoff energy for multigroup structure
-Ef_max = 50.
-Ef_min = 0.01
+g = 2
+g_f = 2
+l = 100
 
-g = 2 # initial photon energy group index
-g_f = 2 # outgoing photon energy group index
-l = 0 # Legendre moment order
-
-E_max = keV_to_mec2(E_max) # keV → mₑc²
-E_min = keV_to_mec2(E_min) # keV → mₑc²
-Ef_max = keV_to_mec2(Ef_max) # keV → mₑc²
-Ef_min = keV_to_mec2(Ef_min) # keV → mₑc²
+E_max = keV_to_mec2(E_max_keV)
+E_min = keV_to_mec2(E_min_keV)
+Ef_max = keV_to_mec2(Ef_max_keV)
+Ef_min = keV_to_mec2(Ef_min_keV)
 
 _, Zi, Ui, _, _, _ = electron_subshells(Z,false)
 Jio = inverse_mₑc²_units_orbital_compton_profiles(Z)
@@ -74,53 +73,61 @@ function print_E_structure(E_structure)
     end
 end
 
-print_E_structure(E_grid)
+#print_E_structure(E_grid)
 print_E_structure(Ef_grid)
 
 
 
-function Ef_integral(l, g_f, Ef_grid, E, Ui, Jio)
+function Ef_integral(l, g_f, Ef_grid, E, Zi, Ui, Jio)
     
+    if E < Ef_grid[g_f + 1]
+        println("Initial photon energy E=$(mec2_to_keV(E)) keV is less than the lower bound Ef=$(mec2_to_keV(Ef_grid[g_f + 1])) keV of outgoing photon energy group g'=$g_f .")
+        return 0.0
+    end
+        
     group_index_upper_bound_f = g_f
     group_index_lower_bound_f = g_f + 1
 
-    lower_bound = Ef_grid[group_index_lower_bound_f]
+    Ef_lower_bound = Ef_grid[group_index_lower_bound_f]
 
-    upper_bound = min(Ef_grid[group_index_upper_bound_f], E)
+    # final photon energy cannot exceed initial photon energy
+    Ef_upper_bound = min(Ef_grid[group_index_upper_bound_f], E)
 
     int, err = quadgk(Ef -> begin
-        legendre_moment(l, E, Ef, Ui, Jio)
-    end, lower_bound, upper_bound)
+        legendre_moment(l, E, Ef, Zi, Ui, Jio)
+    end, Ef_lower_bound, Ef_upper_bound)
     
     return int
 
 end
 
 
-function E_integral(l, g, g_f, E_grid, Ef_grid, Ui, Jio)
+function E_integral(l, g, g_f, E_grid, Ef_grid, Zi, Ui, Jio)
     
-    group_index_upper_bound_f = g
-    group_index_lower_bound_f = g + 1
+    group_index_upper_bound = g
+    group_index_lower_bound = g + 1
 
-    lower_bound = E_grid[group_index_lower_bound_f]
+    E_lower_bound = E_grid[group_index_lower_bound]
 
-    upper_bound = E_grid[group_index_upper_bound_f]
+    E_upper_bound = E_grid[group_index_upper_bound]
 
     int, err = quadgk(E -> begin
-        Ef_integral(l, g_f, Ef_grid, E, Ui, Jio)
-    end, lower_bound, upper_bound)
+        Ef_integral(l, g_f, Ef_grid, E, Zi, Ui, Jio)
+    end, E_lower_bound, E_upper_bound)
 
     return int
 
 end
 
 
-function deltaEf(g_f, Ef_grid)
+function deltaEf(g, E_grid)
 
-    return Ef_grid[g_f] - Ef_grid[g_f+1]
+    return E_grid[g] - E_grid[g+1]
 
 end
 
-group_legendre_moment = E_integral(l, g, g_f, E_grid, Ef_grid, Ui, Jio) / deltaEf(g_f, Ef_grid)
+#group_legendre_moment = E_integral(l, g, g_f, E_grid, Ef_grid, Zi, Ui, Jio) / deltaEf(g, E_grid)
+#println("Group Legendre moment for l=$l, g=$g and g_f=$g_f: $group_legendre_moment")
 
-println("Group Legendre moment for l=$l, g=$g and g_f=$g_f: $group_legendre_moment")
+Specific_energy_legendre_moment = Ef_integral(l, g_f, Ef_grid, keV_to_mec2(50), Zi, Ui, Jio)
+println("Legendre moment for l=$l, g_f=$g_f and E=50 keV: $Specific_energy_legendre_moment")
